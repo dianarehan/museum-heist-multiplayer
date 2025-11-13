69aa5afb8e866636ec30efec53f7832dd7b461b7
seifdiea <seifdiea2003@gmail.com>
2025-11-14 01:20:04 +0200

    Kepping the remote and local synced

--- Assets/prefabs/SecurityGuard/scripts/GuardAnimatorController.cs
+++ Assets/prefabs/SecurityGuard/scripts/GuardAnimatorController.cs
@@ -2,15 +2,39 @@ using UnityEngine;
 
 public class GuardAnimatorController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Animator animator;
+    CharacterController controller;
+
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // 1. Get player movement input
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        bool isMoving = (Mathf.Abs(horizontal) > 0.1f || Mathf.Abs(vertical) > 0.1f);
+
+        // 2. Walking
+        animator.SetBool("IsWalking", isMoving);
+
+        // 3. Jumping
+        if (Input.GetButtonDown("Jump"))
+        {
+            animator.SetTrigger("Jump");
+        }
+
+        // 4. Running
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        animator.SetBool("IsRunning", isRunning);
+
+        // 5. Picking up
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            animator.SetTrigger("Pickup");
+        }
     }
 }
