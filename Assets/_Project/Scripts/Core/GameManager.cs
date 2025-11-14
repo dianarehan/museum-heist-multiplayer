636a73205fde2a16da8bd47ae79bcfd433ef3765
diana <daiana.rehan.dr@gmail.com>
2025-11-14 22:04:47 +0200

    Fix Prefab in the resources folder itself

--- Assets/_Project/Scripts/Core/GameManager.cs
+++ Assets/_Project/Scripts/Core/GameManager.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
 using Photon.Pun;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private string guardPrefabName = "GuardPrefab";
-    [SerializeField] private string thiefPrefabName = "ThiefPrefab";
+    [SerializeField] private string guardPrefabName = "NetworkedGuard";
+    [SerializeField] private string thiefPrefabName = "NetworkedThief";
+
+    [Header("Spawn Points")]
     [SerializeField] private Transform guardSpawnPoint;
-    [SerializeField] private Transform thiefSpawnPoint;
+    [SerializeField] private List<Transform> thiefSpawnPoints;
 
     void Start()
     {
@@ -18,7 +21,14 @@ public class GameManager : MonoBehaviour
         }
         else
         {
-            PhotonNetwork.Instantiate(thiefPrefabName, thiefSpawnPoint.position, thiefSpawnPoint.rotation);
+            int actorNumber = PhotonNetwork.LocalPlayer.ActorNumber;
+
+            int spawnIndex = actorNumber - 2;
+            int wrappedIndex = spawnIndex % thiefSpawnPoints.Count;
+
+            Transform spawnPoint = thiefSpawnPoints[wrappedIndex];
+
+            PhotonNetwork.Instantiate(thiefPrefabName, spawnPoint.position, spawnPoint.rotation);
         }
     }
 }
