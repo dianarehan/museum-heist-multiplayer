1f68bfaaa138dff0c9f87a4efb85716a645986a1
seifdiea <seifdiea2003@gmail.com>
2025-11-15 17:56:12 +0200

    Fix: More fixes to the camera and animations to be visible to all playersss

--- Assets/_Project/Scripts/Thief/ThiefCameraSetup.cs
+++ Assets/_Project/Scripts/Thief/ThiefCameraSetup.cs
@@ -1,26 +1,17 @@
 using UnityEngine;
 using Photon.Pun;
+using Unity.Cinemachine;
 
 public class ThiefCameraSetup : MonoBehaviourPun
 {
-    [SerializeField] private Unity.Cinemachine.CinemachineCamera fpCamera;
-    [SerializeField] private AudioListener audioListener;
+    [SerializeField] private CinemachineCamera fpCamera;
+    [SerializeField] private CinemachineCamera tpCamera;   // optional
 
     void Start()
     {
-        if (!photonView.IsMine)
-        {
-            // Disable remote cameras
-            if (fpCamera != null) fpCamera.enabled = false;
-            if (audioListener != null) audioListener.enabled = false;
-        }
-        else
-        {
-            // Enable only my camera
-            if (fpCamera != null) fpCamera.enabled = true;
+        bool mine = photonView.IsMine;
 
-            // Audio should ONLY be for local player
-            if (audioListener != null) audioListener.enabled = true;
-        }
+        if (fpCamera != null) fpCamera.enabled = mine;
+        if (tpCamera != null) tpCamera.enabled = mine;
     }
 }
